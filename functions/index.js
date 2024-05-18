5756864f38d48dfece49cb80e18dfc1c96f1d340
akacaali <126831892+akacaali@users.noreply.github.com>
2024-05-18 04:48:19 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { getStorage } = require('firebase-admin/storage');
-const { getFunctions } = require('firebase-admin/functions');
 admin.initializeApp();
 
 exports.uploadVideo = functions.storage.object().onFinalize(async (object) => {
   const filePath = object.name;
   console.log(`File uploaded: ${filePath}`);
   // Add your video processing logic here
+  console.log('Processing completed for file: ', filePath);
 });
+
